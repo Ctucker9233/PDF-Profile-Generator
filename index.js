9e7bba81bb4de8cb2394207063663b5afedec8f6
Ctucker9233 <court.m.tucker@gmail.com>
2019-11-16 15:30:32 -0800

    reinstalled node packages and got html to output to pdf

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const fs = require("fs"),
-    convertFactory = require('electron-html-to'), 
-    electronWorkers = require('electron-workers')
+const fs = require("fs")
+const convertapi = require('convertapi')('tTi0uXTS08ennqBS');
 const path = require("path");
 const generate = require("./generateHTML");
 const inquirer = require("inquirer");
@@ -11,9 +10,6 @@ const questions = ["What is your Github user name?", "Pick your favorite color?"
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 };
-
-
-
 
 function promptUser() {
     return inquirer.prompt([
@@ -36,6 +32,7 @@ function init() {
         .then(function ({ username, colorchoice }) {
             const color = colorchoice;
             const queryUrl = `https://api.github.com/users/${username}`;
+            let html;
             axios
                 .get(queryUrl)
                 .then(function (res) {
@@ -44,23 +41,20 @@ function init() {
                     html = generate(res.data);
                     console.log(html)
                     writeToFile("profile.html", html)
-                })
-                .then(html => {
-                    console.log(html)
-                    const convert = convertFactory({
-                        converterPath: convertFactory.converters.PDF,
-                        pathToElectron: '\node_modules\.bin\electron.CMD',
-                        allowLocalFilesAccess: true
-                    });
-                    convert({ html: html }, (err, result) => {
-                        if (err) return console.error(err);
-                        result.stream.pipe(fs.createWriteStream(__dirname + "profile.pdf"));
-                        convert.kill(); // necessary if you use the electron-server strategy, see bellow for details
-                    });
+
                 });
-        });
-};
+            convertapi.convert('pdf', { File: './profile.html' })
+                .then(function (result) {
+                    // get converted file url
+                    console.log("Converted file url: " + result.file.url);
 
+                    // save to file
+                    return result.file.save(__dirname + "/profile.pdf");
+                })
+                .then(function (file) {
+                    console.log("File saved: " + file);
+                });
 
-
+        })
+    }
 init();
